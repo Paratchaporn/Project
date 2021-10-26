ef8a6d922a5f0a340c38486e47a3ab2b7115e881
paratchaporn <paratchaporn.u@gmail.com>
2021-10-26 14:36:01 +0700

    Web

---
+++ HTML and CSS toturial/Advance Feature/public/js/firebase.js
@@ -0,0 +1,13 @@
+let firebaseConfig = {
+    // enter your firebase credentials here.
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
+let auth = firebase.auth();
+
+const logoutUser = () => {
+    auth.signOut();
+    location.reload();
+}
